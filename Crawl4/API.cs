cf847f1c5c9888b2791fba8d2513455b234c9ec1
Anumania <jonahmcathcart@gmail.com>
2021-03-04 04:13:57 -0800

    about halfway done with monster clone, print for Players.

--- Crawl4/API.cs
+++ Crawl4/API.cs
@@ -25,6 +25,7 @@ namespace CrawlAPI
         void Awake()
         {
             APIHelpers.Init();
+            MonsterAPI.Init();
             //GameObject a = new GameObject();
             //a.AddComponent<SystemDebug>();
             //GameObject.DontDestroyOnLoad(a);
@@ -46,15 +47,10 @@ namespace CrawlAPI
             Player[] monsters = (Player[])Resources.FindObjectsOfTypeAll(typeof(Player));
             foreach (Player i in monsters)
             {
+                //Console.WriteLine(i.name);
                 if(i.name == "Hero")
                 {
-                    foreach( var j in i.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
-                    {
-                        if(j.GetCustomAttributes(true).Where(e=>e.GetType().Name == "SerializeField").FirstOrDefault() != null)
-                        {
-                            Console.WriteLine(j.Name + ":" + j.GetValue(i));
-                        }
-                    }
+                    APIHelpers.PrintPlayer(i);   
                 }
             }
             /*[] weapons = (Weapon[])Resources.FindObjectsOfTypeAll(typeof(Weapon));
@@ -67,11 +63,28 @@ namespace CrawlAPI
     }
     public static class MonsterAPI //used to add monsters. probably not gonna work well, replacing is probably better.
     {
+        public static Player[] monsters;
+        public static void Init()
+        {
+            monsters = (Player[])Resources.FindObjectsOfTypeAll(typeof(Player));
+        }
+
+        public static Player GetMonster(string monsterName) //this does not work with mod added monsters!
+        {
+            return monsters.Where(e => e.name == "EnemySkeletonPirate").FirstOrDefault();
+        }
+
         public class EvolveCostOverride
         {
+            public EvolveCostOverride(GameObject evolveTo, float _price = 0)
+            {
+                price = _price;
+                m_evolveTo = evolveTo;
+            }
             public GameObject m_evolveTo;
             public float price;
         }
+        
     }
     public class CustomMonster
     {
@@ -79,23 +92,90 @@ namespace CrawlAPI
         {
 
         }
+
+        public void SetToDefaults()
+        {
+            Player hero = MonsterAPI.GetMonster("Hero");
+            m_evolveTo = new Player[] { MonsterAPI.GetMonster("EnemySkeletonPirate"), MonsterAPI.GetMonster("EnemyGnome")};
+            m_evolveToCostOverride = new MonsterAPI.EvolveCostOverride[]{
+                new MonsterAPI.EvolveCostOverride(MonsterAPI.GetMonster("EnemySkeletonPirate").gameObject),
+                new MonsterAPI.EvolveCostOverride(MonsterAPI.GetMonster("EnemyGnome").gameObject)
+                };
+            m_animationIdle = (exSpriteAnimClip)hero.GetType().GetField("m_animationIdle").GetValue(hero);
+            m_animationRun = (exSpriteAnimClip)hero.GetType().GetField("m_animationRun").GetValue(hero);
+            m_animationDead = (exSpriteAnimClip)hero.GetType().GetField("m_animationDead").GetValue(hero);
+            m_animationKnockback = (exSpriteAnimClip)hero.GetType().GetField("m_animationKnockback").GetValue(hero);
+            m_animationStatue = (exSpriteAnimClip)hero.GetType().GetField("m_animationStatue").GetValue(hero);
+            m_shadowPrefab = (PlayerShadow)hero.GetType().GetField("m_shadowPrefab").GetValue(hero);
+            m_spawnOnHit = (DamageEffectData[])hero.GetType().GetField("m_spawnOnHit").GetValue(hero);
+            m_spawnOnDeath = (DamageEffectData[])hero.GetType().GetField("m_spawnOnDeath").GetValue(hero);
+
+            m_attackOrderType = (Attacks.eAttackOrderType)hero.GetType().GetField("m_attackOrderType").GetValue(hero);
+            m_comboResetTime = (float)hero.GetType().GetField("m_comboResetTime").GetValue(hero);
+            m_showDirectionIndicator = true;
+            m_hasSpecialAttack = (bool)hero.GetType().GetField("m_hasSpecialAttack").GetValue(hero);
+            m_specialAttack = (AttackData)hero.GetType().GetField("m_specialAttack").GetValue(hero);
+
+        }
         //Type
         public bool m_isHero = false;
         public bool m_isGhost = false;
+
         //XP
         public MinMaxRange m_xpOnKill = new MinMaxRange(3f, 4f);
+
         //Evolve
         public float m_evolveCost = 1f;
         public Player[] m_evolveTo; //fill
         public MonsterAPI.EvolveCostOverride[] m_evolveToCostOverride; //fill
+
         //Visuals
-        public exSpriteAnimClip m_animationSpawn; //fill
+        public exSpriteAnimClip m_animationSpawn; //does not need to be filled
         public exSpriteAnimClip m_animationIdle; //fill
         public exSpriteAnimClip m_animationRun; //fill
         public exSpriteAnimClip m_animationDead; //fill
         public exSpriteAnimClip m_animationKnockback; //fill
         public exSpriteAnimClip m_animationStatue; //fill 
         public PlayerShadow m_shadowPrefab; //fill
+        public bool allowRedShadow = true;
+        public DamageEffectData[] m_spawnOnHit; //fill
+        public DamageEffectData[] m_spawnOnDeath; //fill
+        public bool m_bloodEffectsEnabled = true;
+        public bool m_delayGhostSpawnOnDeath = false; //not fill
+
+        //Attacks
+        public Vector2 m_attackSpawnNode = Vector2.right;
+        public Attacks.eAttackOrderType m_attackOrderType;
+        public float m_comboResetTime;
+        public bool m_showDirectionIndicator;
+        public bool m_specialCancelsCooldown = true;
+        public List<AttackData> m_attacks = new List<AttackData>(1);
+        public bool m_hasSpecialAttack;
+        public float m_specialAttackChargeTime = 5f;
+        public AttackData m_specialAttack;
+        public List<DamageEffectData> m_attackDamageEffects = new List<DamageEffectData>(1);
+        public Powerup m_attackPowerupPrefab;
+        public bool m_holdToAim;
+
+        //Knockback
+        public float m_knockbackSpeed = 200f;
+        public float m_knockbackTime = 0.3f;
+        public bool m_knockbackCancelsAttack;
+
+        //Sound
+        public AudioCue m_soundHit;
+        public AudioCue m_soundDie;
+
+        //Portrait
+        public bool m_portraitEnabled = true;
+        public Vector2 m_portraitOffset = Vector2.zero;
+        public Vector2 m_portraitOffsetEvolve = Vector2.zero;
+        public bool m_portraitFlipped;
+        public exSpriteAnimClip m_portraitAnim;
+
+        //Bots
+        public float m_botTargetChance = 1f; //If less than 1 then there'll be a chance that hero  won't prioritise this as a target above others, lower the value, less the chance
+        
 
     }
     public class WeaponAPI //used to add weapons. will work better probably.
@@ -120,6 +200,50 @@ namespace CrawlAPI
         public static MethodDefinition FindMethod(string type, string method)
         {
             return AssemblyCS.MainModule.GetType(type).Methods.Where(e => { return e.Name == method; }).First();
+        }
+        public static void RecursivePrintFields(int howMuch) //pog idea
+        {
+
+        }
+        public static void PrintPlayer(Player i)
+        {
+            foreach (var j in i.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                if (j.GetCustomAttributes(true).Where(e => e.GetType().Name == "SerializeField").FirstOrDefault() != null)
+                {
+                    Console.WriteLine(j.Name + ":" + j.GetValue(i));
+                    if (j.GetValue(i) != null && j.GetValue(i).GetType() == typeof(exSpriteAnimClip))
+                    {
+                        foreach (var m in j.GetValue(i).GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+                        {
+                            var val = m.GetValue(j.GetValue(i));
+                            Console.WriteLine("\t" + m.Name + ":" + val);
+                            if (m.Name == "frameInfos")
+                            {
+
+                                foreach (exSpriteAnimClip.FrameInfo val2 in (List<exSpriteAnimClip.FrameInfo>)val)
+                                {
+
+                                    Console.WriteLine("\t\t" + "textureGUID" + ":" + val2.textureGUID);
+                                    Console.WriteLine("\t\t" + "length" + ":" + val2.length);
+                                    Console.WriteLine("\t\t" + "atlas" + ":" + val2.atlas);
+                                    Console.WriteLine("\t\t" + "index" + ":" + val2.index);
+                                }
+                            }
+                            else if (m.Name == "eventInfos") //this should work, idk though
+                            {
+                                foreach (exSpriteAnimClip.EventInfo val2 in (List<exSpriteAnimClip.EventInfo>)val)
+                                {
+                                    foreach (var val3 in val2.GetType().GetFields())
+                                    {
+                                        Console.WriteLine("\t\t" + val3.Name + ":" + val3.GetValue(val2));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
